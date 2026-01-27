b1999bf96c6825732798482ec42b22e773ebe7f9
Bernardo Cornejo <Bernar2@gmail.com>
2026-01-27 00:08:19 -0300

    OK

--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,3 +1,20 @@
+import { CustomPagination } from '@/components/custom/CustomPagination';
+import { products } from '@/mocks/products.mock';
+import { CustomJumbotrom } from '@/shop/components/CustomJumbotrom';
+import { ProductsGrid } from '@/shop/components/ProductsGrid';
+import { useParams } from 'react-router';
+
 export const GenderPage = () => {
-  return <div>GenderPage</div>;
+  const { gender } = useParams();
+
+  const genderLabel =
+    gender === 'men' ? 'Hombre' : gender === 'women' ? 'Mujeres' : 'Niños';
+
+  return (
+    <>
+      <CustomJumbotrom title={`Productos para ${genderLabel}`} />
+      <ProductsGrid products={products} />
+      <CustomPagination totalPages={7} />
+    </>
+  );
 };
--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,3 +1,14 @@
+import { CustomPagination } from '@/components/custom/CustomPagination';
+import { products } from '@/mocks/products.mock';
+import { CustomJumbotrom } from '@/shop/components/CustomJumbotrom';
+import { ProductsGrid } from '@/shop/components/ProductsGrid';
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <CustomJumbotrom title="Todos los productos" />
+      <ProductsGrid products={products} />
+      <CustomPagination totalPages={7} />
+    </>
+  );
 };
